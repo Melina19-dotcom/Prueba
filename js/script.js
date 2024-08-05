8664a81ead6e6919e4b9102b01a901e7bbce5824
Melina <luzmarrufo3007@gmail.com>
2024-08-05 18:56:22 -0500

    agregando archivos

---
+++ js/script.js
@@ -0,0 +1,22 @@
+/*var i = 0;
+for(i;i<5;i++){
+    console.log(i)
+}
+*/
+/*
+for(var i = 1; i<10; i+=2){
+    console.log(i)
+}
+*/
+/*for(var i = 2; i<11; i+=2){
+    console.log(i)
+}
+*/
+/*
+for(var i = 1; i<21; i+=1){
+    console.log(i);
+}
+*/
+for(var i=0; i<8; i++){
+    console.log("Hola")
+}
