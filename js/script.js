0218ec58e6be42b67f579fe49161b0789022b0a8
Melina <luzmarrufo3007@gmail.com>
2024-08-05 19:40:00 -0500

    agregando codigo al archivo script

--- js/script.js
+++ js/script.js
@@ -17,6 +17,17 @@ for(var i = 1; i<21; i+=1){
     console.log(i);
 }
 */
+/*
 for(var i=0; i<8; i++){
     console.log("Hola")
+}
+*/
+var paises = ["Perú","Colombia","Alemania","Canadá"]
+var cantidad = paises.length;
+console.log(cantidad);
+
+console.log(paises[3]);
+
+for(var i=0; i<cantidad ; i++){
+    console.log(paises[i]);
 }
